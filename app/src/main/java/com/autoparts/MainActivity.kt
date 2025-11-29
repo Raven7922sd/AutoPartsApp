6510d237ccbe2027e65b6b59e05d328ea9b6e41f
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:37:13 -0400

    Main actualizado

--- app/src/main/java/com/autoparts/MainActivity.kt
+++ app/src/main/java/com/autoparts/MainActivity.kt
@@ -6,42 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.autoparts.presentation.navigation.AppNavHost
 import com.autoparts.ui.theme.AutoPartsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AutoPartsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            AutoPartsAppTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navControler = rememberNavController()
+                    Scaffold {innerpadding ->
+                        AppNavHost(
+                            navController = navControler,
+                            modifier = Modifier.padding(innerpadding)
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutoPartsAppTheme {
-        Greeting("Android")
     }
 }
