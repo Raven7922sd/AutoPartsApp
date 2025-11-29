a7c6d962a07312131c3072cb06caaca62cd516ab
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-29 15:27:18 -0400

    UsuariosMappers modificado

--- app/src/main/java/com/autoparts/Data/Mappers/UsuarioMapper.kt
+++ app/src/main/java/com/autoparts/Data/Mappers/UsuarioMapper.kt
@@ -1,16 +1,51 @@
 package com.autoparts.Data.Mappers
 
-import com.autoparts.Data.Remote.Dto.UsuariosDto
+import com.autoparts.Data.Remote.Dto.CreateUserDto
+import com.autoparts.Data.Remote.Dto.LoginDto
+import com.autoparts.Data.Remote.Dto.UpdateUserDto
+import com.autoparts.Data.Remote.Dto.UserDto
+import com.autoparts.dominio.model.CreateUser
+import com.autoparts.dominio.model.LoginUser
+import com.autoparts.dominio.model.UpdateUser
 import com.autoparts.dominio.model.Usuarios
 
-fun Usuarios.toDto() : UsuariosDto = UsuariosDto(
-    usuarioId =  usuarioId,
+fun UserDto.toDomain(): Usuarios = Usuarios(
+    id = id,
     userName = userName,
+    email = email,
+    phoneNumber = phoneNumber,
+    emailConfirmed = emailConfirmed,
+    roles = roles
+)
+
+fun Usuarios.toDto(): UserDto = UserDto(
+    id = id,
+    userName = userName,
+    email = email,
+    phoneNumber = phoneNumber,
+    emailConfirmed = emailConfirmed,
+    phoneNumberConfirmed = false,
+    twoFactorEnabled = false,
+    lockoutEnabled = false,
+    lockoutEnd = null,
+    accessFailedCount = 0,
+    roles = roles
+)
+
+fun CreateUser.toDto(): CreateUserDto = CreateUserDto(
+    email = email,
+    password = password,
+    phoneNumber = phoneNumber
+)
+
+fun LoginUser.toDto(): LoginDto = LoginDto(
+    email = email,
     password = password
 )
 
-fun UsuariosDto.toDomain(): Usuarios = Usuarios(
-    usuarioId = usuarioId,
-    userName = userName,
-    password = password
+fun UpdateUser.toDto(): UpdateUserDto = UpdateUserDto(
+    email = email,
+    phoneNumber = phoneNumber,
+    currentPassword = currentPassword,
+    newPassword = newPassword
 )
