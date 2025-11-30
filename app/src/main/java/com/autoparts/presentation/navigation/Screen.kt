456f427f1b72c4744c7186167b22d6ca39cf453d
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-30 14:53:51 -0400

    NavigationScreen actualizado

--- app/src/main/java/com/autoparts/presentation/navigation/Screen.kt
+++ app/src/main/java/com/autoparts/presentation/navigation/Screen.kt
@@ -7,8 +7,17 @@ sealed class Screen(val route: String){
     }
     data object Login: Screen("login_screen")
     data object Productos: Screen("productos_screen")
+    data object Categorias: Screen("categorias_screen")
     data object ProductoDetalle: Screen("producto_detalle/{productoId}"){
         const val ARG = "productoId"
         fun createRoute(productoId: Int) = "producto_detalle/$productoId"
     }
+    data object Carrito: Screen("carrito_screen")
+    data object Checkout: Screen("checkout_screen")
+    data object Ventas: Screen("ventas_screen")
+    data object VentaDetalle: Screen("venta_detalle/{ventaId}"){
+        const val ARG = "ventaId"
+        fun createRoute(ventaId: Int) = "venta_detalle/$ventaId"
+    }
+    data object Perfil: Screen("perfil_screen")
 }
