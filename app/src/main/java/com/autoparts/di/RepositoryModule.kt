842f5c94e32f43335ba95e4566cff8cd514035ba
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-11-30 14:54:04 -0400

    RepositoryModule actualizado

--- app/src/main/java/com/autoparts/di/RepositoryModule.kt
+++ app/src/main/java/com/autoparts/di/RepositoryModule.kt
@@ -2,8 +2,12 @@ package com.autoparts.di
 
 import com.autoparts.Data.Repository.ProductoRepositoryImpl
 import com.autoparts.Data.Repository.UsuarioRepositoryImpl
+import com.autoparts.Data.Repository.CarritoRepositoryImpl
+import com.autoparts.Data.Repository.VentasRepositoryImpl
 import com.autoparts.dominio.repository.ProductoRepository
 import com.autoparts.dominio.repository.UsuarioRepository
+import com.autoparts.dominio.repository.CarritoRepository
+import com.autoparts.dominio.repository.VentasRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +29,16 @@ abstract class RepositoryModule {
     abstract fun bindProductoRepository(
         impl: ProductoRepositoryImpl
     ): ProductoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCarritoRepository(
+        impl: CarritoRepositoryImpl
+    ): CarritoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindVentasRepository(
+        impl: VentasRepositoryImpl
+    ): VentasRepository
 }
