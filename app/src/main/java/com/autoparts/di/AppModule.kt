e0521acd1601c6ae98ea7fff73a3705b9f35fd52
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-12-07 19:37:27 -0400

    feat ✨, Servicios, Citas, Manejo de sesión y CarritoLocal agregados a AppModule

--- app/src/main/java/com/autoparts/di/AppModule.kt
+++ app/src/main/java/com/autoparts/di/AppModule.kt
@@ -1,13 +1,20 @@
 package com.autoparts.di
 
 import android.content.Context
-import com.autoparts.Data.Remote.AuthInterceptor
-import com.autoparts.Data.Remote.CarritoApiService
-import com.autoparts.Data.Remote.ProductosApiService
-import com.autoparts.Data.Remote.UsuariosApiService
-import com.autoparts.Data.Remote.api.VentasApiService
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import androidx.room.Room
+import com.autoparts.data.remote.interceptor.AuthInterceptor
+import com.autoparts.data.remote.api.CarritoApiService
+import com.autoparts.data.remote.api.ProductosApiService
+import com.autoparts.data.remote.api.UsuariosApiService
+import com.autoparts.data.remote.api.VentasApiService
+import com.autoparts.data.remote.api.ServiciosApiService
+import com.autoparts.data.remote.api.CitasApiService
+import com.autoparts.data.local.database.AutoPartsDatabase
+import com.autoparts.data.local.dao.UsuarioDao
+import com.autoparts.data.local.manager.CarritoLocalManager
+import com.autoparts.data.local.manager.SessionManager
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,12 +23,32 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAutoPartsDatabase(
+        @ApplicationContext context: Context
+    ): AutoPartsDatabase {
+        return Room.databaseBuilder(
+            context,
+            AutoPartsDatabase::class.java,
+            "autoparts_database"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUsuarioDao(database: AutoPartsDatabase): UsuarioDao {
+        return database.usuarioDao()
+    }
 
     @Provides
     @Singleton
@@ -35,25 +62,26 @@ object AppModule {
             .addInterceptor(authInterceptor)
             .build()
     }
-    @Provides
-    @Singleton
-    fun provideMoshi(): Moshi =
-        Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
 
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit =
+    fun provideGson(): Gson =
+        GsonBuilder()
+            .setLenient()
+            .create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit =
         Retrofit.Builder()
             .baseUrl("https://autopartsap1-production.up.railway.app/")
             .client(okHttpClient)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
     @Provides
     @Singleton
-    fun provideJugadorApiService(retrofit: Retrofit): UsuariosApiService {
+    fun provideUsuariosApiService(retrofit: Retrofit): UsuariosApiService {
         return retrofit.create(UsuariosApiService::class.java)
     }
 
@@ -74,4 +102,28 @@ object AppModule {
     fun provideVentasApiService(retrofit: Retrofit): VentasApiService {
         return retrofit.create(VentasApiService::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideServiciosApiService(retrofit: Retrofit): ServiciosApiService {
+        return retrofit.create(ServiciosApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCitasApiService(retrofit: Retrofit): CitasApiService {
+        return retrofit.create(CitasApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSessionManager(
+        @ApplicationContext context: Context
+    ): SessionManager = SessionManager(context)
+
+    @Provides
+    @Singleton
+    fun provideCarritoLocalManager(
+        @ApplicationContext context: Context
+    ): CarritoLocalManager = CarritoLocalManager(context)
 }
