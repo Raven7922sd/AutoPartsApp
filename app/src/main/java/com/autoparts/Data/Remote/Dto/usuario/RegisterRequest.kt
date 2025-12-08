600cac76af90a40c78a134a1cf40b9cc0d9e734d
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-12-07 23:04:17 -0400

    feat ✨, RegisterRequest Agregado

---
+++ app/src/main/java/com/autoparts/Data/Remote/Dto/usuario/RegisterRequest.kt
@@ -0,0 +1,12 @@
+package com.autoparts.data.remote.dto.usuario
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterRequest(
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("phoneNumber")
+    val phoneNumber: String? = null
+)
