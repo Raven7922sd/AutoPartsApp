d243f3ae8f311c24a7534f71014df333143214eb
Johan Estiven Reinoso <johan_reinoso@ucne.edu.do>
2025-12-10 02:29:14 -0400

    fix 🐛, Complejidad cognitiva reducida

--- app/src/main/java/com/autoparts/presentation/crearcita/CrearCitaScreen.kt
+++ app/src/main/java/com/autoparts/presentation/crearcita/CrearCitaScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.autoparts.ui.theme.AutoPartsAppTheme
 import java.text.SimpleDateFormat
 import java.util.Calendar
